c75ab531dfa7a37cf0c1ff1c5b62dba7a9ce203b
Yakov Manuilov <110241091+IooEWREYooI@users.noreply.github.com>
2022-11-08 20:30:16 +0300

    Delete BotConfig.java

--- src/main/java/telegrambots/BotConfig.java
+++
@@ -1,9 +0,0 @@
-package telegrambots;
-
-public class BotConfig {
-    public final static String BOT_TOKEN = "5523641768:AAH7xppl8yK1bDXLpKaJiPj8UsZKM1_iFOA";
-    public final static String BOT_NAME = "Math_Prof_Formuls_bot";
-    public final static String BOT_TOKEN_TEST = "5782481564:AAEqgsSDhMNgRLQuSTSuTMp9jg4-SE5Z5fw";
-    public final static String BOT_NAME_TEST = "userAboba228_bot";
-
-}
